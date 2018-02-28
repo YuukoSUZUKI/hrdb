773a5ac14be084476d61d5ebee8e11bee4babd88
YuukoSUZUKI <yu-suzuki@sts-inc.co.jp>
2018-02-28 16:51:47 +0000

    スキル仮実装

--- app/javascript/packs/views/skill/detail.vue
+++ app/javascript/packs/views/skill/detail.vue
@@ -1,3 +1,4 @@
+
 <template>
 	<section>
 	  <!-- 基本情報 -->
@@ -21,27 +22,13 @@
 	  <el-collapse v-model="activeNames">
       <el-collapse-item v-for="(category, key, index) in dynamicCategories" 
           :key="category.id" :title="category.name" :name="category.id">
-
         <el-tag
           :key="tag"
           v-for="tag in category.items"
-          closable
           :disable-transitions="false"
-          @close="handleClose(category.items,tag)">
+          >
           {{tag}}
         </el-tag>
-        <el-input
-          class="input-new-tag"
-          v-if="category.inputVisible"
-          v-model="category.inputValue"
-          ref="saveTagInput"
-          size="mini"
-          @keyup.enter.native="handleInputConfirm(category.items)"
-          @blur="handleInputConfirm(category.items)"
-        >
-        </el-input>
-        <el-button v-else class="button-new-tag" size="small" @click="showInput(category)">+ 追加</el-button>        
-
       </el-collapse-item>
     </el-collapse>
 
@@ -54,55 +41,20 @@
     data() {
       return {
         //アコーディオンとタグのデータ
-        dynamicCategories: [ {id:'1',name:'言語', inputVisible:false ,inputValue:'' ,items:['java','C#','PHP','VB']},
-                              {id:'2', name:'フレームワーク', inputVisible:false ,inputValue:'' , items:['spring','struts2','cakePHP','.net','iBatis','dbflute']},
-                              {id:'3', name:'OS・DB・ミドルウェア', inputVisible:false ,inputValue:'' ,items:['Linux','Oracle','MySQL','postgresql','Apache','Tomcat','redis','memcached']},
-                              {id:'4', name:'業種', inputVisible:false ,inputValue:'' ,items:['人材紹介','保険代理店','小売','流通','不動産']},
-                              {id:'5', name:'ポジション', inputVisible:false ,inputValue:'' ,items:['PM','サブリーダー']},
-                              {id:'6', name:'フェーズ', inputVisible:false ,inputValue:'' ,items:['要件定義','基本設計(外部設計)','詳細設計(内部設計)','結合テスト','総合テスト']},
-                              {id:'7', name:'資格', inputVisible:false ,inputValue:'' ,items:['PMP','ITIL','Oracle Gold']},
+        dynamicCategories: [ {id:'1',name:'言語', items:['java','C#','PHP','VB']},
+                              {id:'2', name:'フレームワーク',  items:['spring','struts2','cakePHP','.net','iBatis','dbflute']},
+                              {id:'3', name:'OS・DB・ミドルウェア', items:['Linux','Oracle','MySQL','postgresql','Apache','Tomcat','redis','memcached']},
+                              {id:'4', name:'業種', items:['人材紹介','保険代理店','小売','流通','不動産']},
+                              {id:'5', name:'ポジション', items:['PM','サブリーダー']},
+                              {id:'6', name:'フェーズ', items:['要件定義','基本設計(外部設計)','詳細設計(内部設計)','結合テスト','総合テスト']},
+                              {id:'7', name:'資格', items:['PMP','ITIL','Oracle Gold']},
                             ],
         //アコーディオンを最初から全て展開する
         activeNames: ['1','2','3','4','5','6','7'],
         
-        //タグ
-        // inputVisible: false,
-        // inputValue: ''
-        
       };
     },
     methods: {
-      //タグ削除
-      handleClose(items,tag) {
-        console.log(items);
-        console.log(tag);
-        items.splice(items.indexOf(tag), 1);
-      },
-      
-      
-      showInput(category) {
-        category.inputVisible = true;
-        this.$nextTick(_ => {
-          let index = this.dynamicCategories.indexOf(category) -1;
-          console.log(index);
-          
-          category.inputValue.focus();
-          
-          // let input = this.$refs.saveTagInput[index];
-          // input.focus();
-          
-          // this.$refs.saveTagInput.$refs.input.focus();
-        });
-      },
-      //タグ新規追加
-      handleInputConfirm(category) {
-        let inputValue = category.inputValue;
-        if (inputValue) {
-          category.items.push(inputValue);
-        }
-        category.inputVisible = false;
-        category.inputValue = '';
-      }      
     }
   }
 </script>
