5436d524ed948e57803bdc2330847b545cb456d7
YuukoSUZUKI <yu-suzuki@sts-inc.co.jp>
2018-03-10 01:46:05 +0000

    ログイン画面デザイン

--- app/javascript/packs/views/Login.vue
+++ app/javascript/packs/views/Login.vue
@@ -1,16 +1,20 @@
 <template>
-  <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
-    <h3 class="title">HRDB</h3>
-    <el-form-item prop="account">
-      <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="アカウント"></el-input>
-    </el-form-item>
-    <el-form-item prop="checkPass">
-      <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="パスワード"></el-input>
-    </el-form-item>
-    <el-form-item style="width:100%;">
-      <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">ログイン</el-button>
-    </el-form-item>
-  </el-form>
+  <div class="login-background">
+    <el-form :model="ruleForm2" :rules="rules2" ref="ruleForm2" label-position="left" label-width="0px" class="demo-ruleForm login-container">
+      <div class="title"><i class="el-icon-search"></i>meru
+        <span class="subtitle">“クメル”で才能を検索</span>
+      </div>
+      <el-form-item prop="account">
+        <el-input type="text" v-model="ruleForm2.account" auto-complete="off" placeholder="アカウント"></el-input>
+      </el-form-item>
+      <el-form-item prop="checkPass">
+        <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off" placeholder="パスワード"></el-input>
+      </el-form-item>
+      <el-form-item style="width:100%;">
+        <el-button type="primary" style="width:100%;" @click.native.prevent="handleSubmit2" :loading="logining">ログイン</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
@@ -74,22 +78,34 @@
 </script>
 
 <style lang="scss" scoped>
+  .login-background {
+    position: absolute;
+    width: 100vw;
+    height: 100vh;
+    background: linear-gradient(to right, rgba(166, 255, 203, 0.9), rgba(18, 216, 250, 0.8), rgba(31, 162, 255, 0.9)) ,url(../assets/bg-login.jpg) center;
+    background-size:cover;
+    background-repeat:no-repeat;
+    
+  }
   .login-container {
-    /*box-shadow: 0 0px 8px 0 rgba(0, 0, 0, 0.06), 0 1px 0px 0 rgba(0, 0, 0, 0.02);*/
-    -webkit-border-radius: 5px;
+
+
     border-radius: 5px;
-    -moz-border-radius: 5px;
     background-clip: padding-box;
     margin: 180px auto;
     width: 350px;
     padding: 35px 35px 15px 35px;
     background: #fff;
-    border: 1px solid #eaeaea;
-    box-shadow: 0 0 25px #cac6c6;
     .title {
       margin: 0px auto 40px auto;
       text-align: center;
       color: #505458;
+      font-size : 40px;
+    }
+    .subtitle {
+      display : block;
+      font-size : 12px;
+      margin-top : 0.5rem;
     }
     .remember {
       margin: 0px 0px 35px 0px;
